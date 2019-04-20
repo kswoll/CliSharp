20887ef5784964a2505887d87429f719863768e6
Kirk Woll <kirk.woll@gmail.com>
2019-04-20 12:29:35 -0700

    BulkRename

---
+++ BulkRename/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace BulkRename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromPattern = args[0];
+            var toPattern = args[1];
+            var folder = Environment.CurrentDirectory;
+
+            foreach (var file in Directory.EnumerateFiles(folder, fromPattern, SearchOption.AllDirectories))
+            {
+                var toDirectory = Path.GetDirectoryName(file);
+                var toFileWithoutExtension = Path.GetFileNameWithoutExtension(file);
+                var toFile = Path.Combine(toDirectory, $"{toFileWithoutExtension}.{toPattern}");
+                Console.WriteLine(toFile);
+                File.Move(file, toFile);
+            }
+        }
+    }
+}
